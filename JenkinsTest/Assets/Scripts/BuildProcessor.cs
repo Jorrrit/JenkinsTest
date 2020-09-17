5532eaefe6fb4e6b68b7334ae10436714544127f
Jorrrit <jorrit@codeglue.com>
2020-09-17 11:27:18 +0200

    Added some code

---
+++ JenkinsTest/Assets/Scripts/BuildProcessor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+namespace Assets.Editor
+{
+	public static class BuildProcessor
+	{
+		/// <summary>
+		/// Function is intended to be run from command line
+		/// </summary>
+        
+		[MenuItem("BuildProcessor/Build VR Driver")]
+		static void BuildJenkinsTest()
+		{
+			BuildPipeline.BuildPlayer
+                (
+				new EditorBuildSettingsScene[] { new EditorBuildSettingsScene("Assets/Scenes/Driver.unity", true) },
+				"c:/outdir/",
+				 BuildTarget.StandaloneWindows,
+				 BuildOptions.None
+				);
+		}
+	}
+}
